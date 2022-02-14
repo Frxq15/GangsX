69c1e6a44e7e7f3755d1c2805613b78b2973562e
Frxq15 <cartwrightdan15@gmail.com>
2022-02-14 01:22:26 +0000

    added subcommand class

--- src/main/java/com/witherfrxq/gangsx/CommandManager/Commands/SubCommands/helpCommand.java
+++ src/main/java/com/witherfrxq/gangsx/CommandManager/Commands/SubCommands/helpCommand.java
@@ -1,4 +1,26 @@
 package com.witherfrxq.gangsx.CommandManager.Commands.SubCommands;
 
-public class helpCommand {
+import com.witherfrxq.gangsx.CommandManager.SubCommand;
+import com.witherfrxq.gangsx.GangsX;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class helpCommand extends SubCommand {
+    private final GangsX plugin;
+    public helpCommand(GangsX plugin) {
+        super("help", "gangsx.command.help", "help <page>", Arrays.asList("commands", "usage"));
+        this.plugin = plugin;
+    }
+
+    @Override
+    public void onCommand(CommandSender sender, Command command, String label, String[] args) {
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        return null;
+    }
 }
--- src/main/java/com/witherfrxq/gangsx/CommandManager/SubCommand.java
+++ src/main/java/com/witherfrxq/gangsx/CommandManager/SubCommand.java
@@ -1,4 +1,40 @@
 package com.witherfrxq.gangsx.CommandManager;
 
-public class SubCommand {
+import com.sun.istack.internal.NotNull;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import java.util.List;
+
+public abstract class SubCommand {
+    private final String command;
+    private final String permission;
+    private final String usage;
+    private final List<String> aliases;
+
+    public SubCommand(String command, String permission, String usage, List<String> aliases) {
+        this.command = command;
+        this.permission = permission;
+        this.usage = usage;
+        this.aliases = aliases;
+    }
+
+    public String getCommand() {
+        return command;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+
+    public String getUsage() {
+        return usage;
+    }
+
+    public List<String> getAliases() {
+        return aliases;
+    }
+    public abstract void onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args);
+
+    public abstract List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args);
 }
